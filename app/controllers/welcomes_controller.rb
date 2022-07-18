b29f6ad729b58725f315ade369c92044a6023b6e
achyut kadam <achyut.k@acldigital.com>
2022-07-18 17:36:15 +0530

    Post page design

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,4 +1,22 @@
 class WelcomesController < ApplicationController
   def index
   end
+
+  def download
+    puts"||||||||||||||||||||||||"
+    puts"||||||||||||||||||||||||"
+    puts params
+    puts"||||||||||||||||||||||||"
+    puts"||||||||||||||||||||||||"
+    redirect_to root_path
+  end
+
+  def send_data_into_another_bucket
+    puts"------------------------"
+    puts"------------------------"
+    puts params
+    puts"------------------------"
+    puts"------------------------"
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
   root "welcomes#index"
+
+  get 'download', action: :download, controller: 'welcomes'
+  get 'send_data_into_another_bucket', action: :send_data_into_another_bucket, controller: 'welcomes'
 end
