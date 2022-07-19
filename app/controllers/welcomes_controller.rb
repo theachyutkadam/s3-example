815037d5702910633176e62bf5aa9b184a86887f
achyut kadam <achyut.k@acldigital.com>
2022-07-19 20:06:05 +0530

    bucket list setup

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,5 +1,9 @@
 class WelcomesController < ApplicationController
   def index
+    region = Rails.application.credentials.aws[:region]
+    access_key = Rails.application.credentials.aws[:access_key_id]
+    secret_access_key = Rails.application.credentials.aws[:secret_access_key]
+    s3 = Aws::S3::Client.new(region: region, access_key_id: access_key, secret_access_key: secret_access_key)
   end
 
   def download
